0947ac85003c302afceb094f2fc8a8b286abd739
ysh443 <ysh443@users.noreply.github.com>
2018-01-30 15:52:32 +0200

    Update JavaScriptBasics.js

--- JavaScriptBasics.js
+++ JavaScriptBasics.js
@@ -1,9 +1,10 @@
+console.clear();
 // function defuinition
 
 var greet = function (person,message) {
 	var greeting= "Hello "+person+", "+message;
   console.log(greeting);
-
+			
 }
 
 greet('Alon','How do you do'); // single line comment
@@ -83,5 +84,108 @@ SayIt.call(person1,' another message')
 
 SayItSimple();// this produces empty strings since invoked on null object
 SayItSimple(person1.FirstName,' evu zale?');
+//same result using .apply() , but delivering all arguments whith array
+SayItSimple.apply(person1,[person1.LastName,' how do you go??']);
 
-console.log();console.log();console.log();console.log();console.log();
+
+// using js onb on jquery obj to set several properties inb one action:
+$('#main').css({
+  color: 'red',
+  border: '5px solid blue'
+});
+
+
+
+/*
+the 'this' keyword imply to diffent objects , depends of the call state:
+1)jQuery tends to take control over its meaning.
+2)In the case of event handlers, this will refer to the element to which you bound the handler;
+3)in the case of iterating over a set of elements in a selection, this will refer to the current element in the iteration.
+*/
+
+var myArray = [1,6,-3,'t' ,"YoYo"]
+var firstItem = myArray["0"];
+
+
+console.log(firstItem);
+console.log(myArray[4]);
+console.log(myArray.length);
+
+for (i = 0; i  < myArray.length ; i++) {
+   console.log("element "+i+" on myArray is "+myArray[i]+'\n');
+}
+
+
+/*  All values on js are TRUE except:
+  undefined (the default value of declared variables that are not assigned a value)
+  null
+  NaN ("not a number")
+  0 (the number zero)
+  '' (an empty string)
+
+*/
+
+if("WHAT"){console.log("All is true");}
+else{ 
+console.log("or not?");
+}
+
+if(!NaN){console.log("A negative check");}
+
+var  notANumber= 'four' - 'five';
+console.log(notANumber);
+if ( notANumber == false ) {
+  // this code will not run!
+  console.log( 'notANumber was falsy' );
+} else {
+  // this code will run
+  console.log( 'notANumber was truthy' );
+}
+
+var foo =1;
+var bar =0;
+var baz =2
+
+var doSomthing1 = function(){return"soWhat"}
+console.log("foo||bar= "+(foo || bar));
+console.log("foo&&baz= "+(foo&&baz));
+console.log("foo||doSomthing1()   "+  (foo||doSomthing1())); // run doSomthing1() only if foo is falsy
+console.log("bar||doSomthing1()   "+  (bar||doSomthing1())); // run doSomthing1() only if foo is falsy
+
+var res1 = foo || bar;
+console.log(res1);
+
+// a ternary conditional operator:
+var dim;
+var propertyName = ( dim === 'width' ) ? 'clientWidth' : 'clientHeight';
+console.log("propertyName="+propertyName);
+
+// naming convertions
+var     _privateVar, $jQaueryObj, SomeObjectConstroctor;
+//starts^_           ^$           ^capital
+
+/*    reserved keywords:
+abstract boolean break byte case catch char class const continue debugger
+default delete do double else enum export extends false final finally float
+for function goto if implements import in instanceof int interface long
+native new null package private protected public return short static super
+switch synchronized this throw throws transient true try typeof var
+volatile void while with*/
+
+// inorder to use keyword as var\function name, use that:
+var myArgs1 = {  arg1:2 , arg2:7 , "interface":"MOOKI" , 'break':1};
+
+
+// be aware of the way js handles numbers accuration:
+console.log(0.0001 + 0.0002);  // outpot is 0.00030000000000000003
+
+// js is loosly-type language thefor dont fail on ambigus litrrals:
+console.log( 'a' + 2 );           // 'a2'
+console.log( '4' + 3 );           // '43'
+console.log( 'five' - '4' );      // NaN (not a number)
+console.log( - '1' );             // -1
+console.log( 1 + true );          // 2
+console.log( 1 == true );         // true
+console.log( 1 === true );        //false
+
+console.log();
